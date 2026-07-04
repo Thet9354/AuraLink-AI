b6af813013dc9121805a63bedb54714b5bc69437
Thet Pine <thetpine254@gmail.com>
2026-07-04 14:48:00 +0800

    Initial Commit

---
+++ AuraLink AI/AuraLink_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraLink_AIApp.swift
+//  AuraLink AI
+//
+//  Created by Phoon Thet Pine on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraLink_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraLink AITests/AuraLink_AITests.swift
@@ -0,0 +1,19 @@
+//
+//  AuraLink_AITests.swift
+//  AuraLink AITests
+//
+//  Created by Phoon Thet Pine on 4/7/26.
+//
+
+import Testing
+@testable import AuraLink_AI
+
+struct AuraLink_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
